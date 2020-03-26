9b780e8d0e5b57b7493cfa8c29acf6bebbcfebc3
Elisa <elisacastaneda0045@gmail.com>
2020-03-26 23:25:58 +0100

    Version-1
    create the index for see the live server and use the console, make the exercise for the arrays, and create the development-strategy file

---
+++ version1.js
@@ -0,0 +1,10 @@
+//how to create an array 
+var todos = ["item 1", "item2", "item3"];
+//how to display a javaScript value
+console.log("My todos:", todos);
+// how to add an new item in an array 
+todos.push("new todos");
+// 
+todos[0] = "changed!";
+// to delete item from array
+todos.splice(0, 1);
